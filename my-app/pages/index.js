76e1a3362576df1decd8a85c154950d8525624c2
Dalt <dbarrychef@gmail.com>
2022-11-10 18:37:59 +0000

    3

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,58 +1,80 @@
-
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
-
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
 export default function Home() {
-  const[walletConnected, setWalletConnected] = useState(false);
+  // walletConnected keep track of whether the user's wallet is connected or not
+  const [walletConnected, setWalletConnected] = useState(false);
+  // joinedWhitelist keeps track of whether the current metamask address has joined the Whitelist or not
+  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+  // loading is set to true when we are waiting for a transaction to get mined
+  const [loading, setLoading] = useState(false);
   const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
-  const web3modal = useRef();
+  const web3ModalRef = useRef();
 
-const getProviderOrSigner = async (needSigner = false) => {
-  try{
-    const provider = await web3modal.current.connect();
-    const web3Provider = new providers.web3Provider(provider);
+  /**
+   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+   * signing capabilities of metamask attached
+   *
+   * @param {*} needSigner - True if you need the signer, default false otherwise
+   */
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
 
-    const {chainId} =  await web3Provider.getNetwork ();
-    if(chainId !==5){ 
+    // If user is not connected to the Goerli network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
       window.alert("Change the network to Goerli");
-      throw new Error("Change the network to Goerli")
+      throw new Error("Change network to Goerli");
     }
-  } catch (err){
-    console.error(err)
-  }
-}
 
-  const connectWallet = async() => {
-    try{
-      await getProviderOrSigner();
-      setWalletConnected(true)
-      checkIfAddressIsWhitelisted();
-      getNumberOfWhitelisted();
-    }catch(err){
-      console.error(err)
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
     }
-  }
+    return web3Provider;
+  };
 
-  useEffect(() =>{
-    if(!walletConnected){
-      web3modal.current = new Web3Modal ({
-      network: "goerli",
-      providerOptions: {},
-      disableInjectedProvider: false,
-      });
-      connectWallet();
+  /**
+   * addAddressToWhitelist: Adds the current connected address to the whitelist
+   */
+  const addAddressToWhitelist = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      // Create a new instance of the Contract with a Signer, which allows
+      // update methods
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      // call the addAddressToWhitelist from the contract
+      const tx = await whitelistContract.addAddressToWhitelist();
+      setLoading(true);
+      // wait for the transaction to get mined
+      await tx.wait();
+      setLoading(false);
+      // get the updated number of addresses in the whitelist
+      await getNumberOfWhitelisted();
+      setJoinedWhitelist(true);
+    } catch (err) {
+      console.error(err);
     }
-  }, [walletConnected,]);
+  };
 
+  /**
+   * getNumberOfWhitelisted:  gets the number of whitelisted addresses
+   */
   const getNumberOfWhitelisted = async () => {
     try {
-      // Get the provider
-      // No need for the Signer here,
+   
       const provider = await getProviderOrSigner();
+      
       const whitelistContract = new Contract(
         WHITELIST_CONTRACT_ADDRESS,
         abi,
@@ -66,10 +88,11 @@ const getProviderOrSigner = async (needSigner = false) => {
       console.error(err);
     }
   };
+
   /**
-   *  Checks if the address is in whitelist
+   * checkIfAddressInWhitelist: Checks if the address is in whitelist
    */
-   const checkIfAddressIsWhitelisted = async () => {
+  const checkIfAddressInWhitelist = async () => {
     try {
     
       const signer = await getProviderOrSigner(true);
@@ -89,31 +112,90 @@ const getProviderOrSigner = async (needSigner = false) => {
       console.error(err);
     }
   };
-  
 
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+
+      checkIfAddressInWhitelist();
+      getNumberOfWhitelisted();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /*
+    renderButton: Returns a button based on the state of the dapp
+  */
+  const renderButton = () => {
+    if (walletConnected) {
+      if (joinedWhitelist) {
+        return (
+          <div className={styles.description}>
+            Thanks for joining the Whitelist!
+          </div>
+        );
+      } else if (loading) {
+        return <button className={styles.button}>Loading...</button>;
+      } else {
+        return (
+          <button onClick={addAddressToWhitelist} className={styles.button}>
+            Join the Whitelist
+          </button>
+        );
+      }
+    } else {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect your wallet
+        </button>
+      );
+    }
+  };
+
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
 
   return (
     <div>
-      <head>
-        <title> WHitelist-Dapp</title>
-        <meta name="description" content="whitelist-Dapp"/>
-      </head>
+      <Head>
+        <title>Whitelist Dapp</title>
+        <meta name="description" content="Whitelist-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to 111
-        </h1>
-        <div className={styles.description}>
-          {numberOfWhitelisted} have already joined the whitelist
-        </div>
-          <div>
-          <img className={styles.image} src="img\cryptocurrency_hero.webp"/>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <div className={styles.description}>
+            Its an NFT collection for developers in Crypto.
           </div>
+          <div className={styles.description}>
+            {numberOfWhitelisted} have already joined the Whitelist
+          </div>
+          {renderButton()}
         </div>
+        <div>
+          <img className={styles.image} src="./crypto-devs.svg" />
+        </div>
+      </div>
 
       <footer className={styles.footer}>
-
+        
       </footer>
     </div>
-    
   );
 }
